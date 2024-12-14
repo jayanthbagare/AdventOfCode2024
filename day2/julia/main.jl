9ac5a511d96983054a332711cd6874ec41408e84
jayanthbagare <jayanth.bagare@gmail.com>
2024-12-14 23:54:45 +0530

    Day 2 commit

--- day2/julia/main.jl
+++ day2/julia/main.jl
@@ -1,30 +1,65 @@
 begin
   arr = []
+  m = []
   safe = 0
   unsafe = 0
-  for line in eachline("input_data")
-    l = parse.(Int32,split(line," "))
-    println(l)
-    # println(abs.(diff(l)))
-    # println(length(findall(x->x>3,abs.(diff(l)))) )
-    if (length(findall(x->x>3,abs.(diff(l)))) > 0)
-      global unsafe += 1
-      # println("Unsafe dur to diff")
-    elseif(0 in abs.(diff(l)))
-      global unsafe += 1
-      # println("Unsafe as diff is zero")
-    elseif(issorted(l,rev=true))
-      global safe += 1
-      # println("Safe as rev sort is true")
-    elseif (issorted(l))
-      global safe += 1
-      # println("Safe as sort is true")
-    else
-      global unsafe += 1  
-      # println("Unsafe as sort is not true")
+  msafe = 0
+  munsafe = 0
+  function safe_unsafe()
+    for line in eachline("input_data")
+      l = parse.(Int32,split(line," "))
+      if (length(findall(x->x>3,abs.(diff(l)))) > 0)
+        global unsafe += 1
+      elseif(0 in abs.(diff(l)))
+        idx = indexin(0,abs.(diff(l)))
+        deleteat!(l,idx)
+        global safe += 1
+        push!(m,l)
+      elseif(issorted(l,rev=true))
+        global safe += 1
+        push!(m,l)
+      elseif (issorted(l))
+        global safe += 1
+        push!(m,l)
+      else
+        if(length(findall(x->x<0, diff(l))) <= 1)
+          global safe += 1  
+          push!(m,l)
+        else
+          global unsafe += 1
+        end
+      end
     end
-    # println("================================")
   end
+  safe_unsafe()
   println("Safe:",safe)
   println("Unsafe:",unsafe)
+  # println("M is ", m)
+  println("M size is ", size(m))
+  for line in m
+    l = line
+    if (length(findall(x->x>3,abs.(diff(l)))) > 0)
+      global munsafe += 1
+    elseif(0 in abs.(diff(l)))
+      idx = indexin(0,abs.(diff(l)))
+      deleteat!(l,idx)
+      global msafe += 1
+      push!(m,l)
+    elseif(issorted(l,rev=true))
+      global msafe += 1
+      push!(m,l)
+    elseif (issorted(l))
+      global msafe += 1
+      push!(m,l)
+    else
+      if(length(findall(x->x<0, diff(l))) <= 1)
+        global msafe += 1  
+        push!(m,l)
+      else
+        global munsafe += 1
+      end
+    end
+  end
+  println("Safe:",msafe)
+  println("Unsafe:",munsafe)
 end
